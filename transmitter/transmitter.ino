9f88c87ae28725c62b2aed13f24d0b6108f0a622
elshadsabziyev <79898558+elshadsabziyev@users.noreply.github.com>
2023-11-27 11:38:02 +0400

    added car controls

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -15,7 +15,7 @@
 // RST             D9           D8
 // 3.3V            3.3V         3.3V
 
-#define servoPin 8            // Set servo pin
+#define servoPin 6            // Set servo pin
 String previousUID = "0";     // Do not change //
 String previousRFIDUID = "0"; // Do not change //
 String fromSerialUID = "0";   // Do not change //
@@ -28,6 +28,12 @@ int optimizationDelay = 50;   // Fine tune this value to optimize performance
 int firstMainLoop = 1;        // Do not change //
 Servo myServo;                // Do not change //
 MFRC522 card(10, 9);          // SDA, RST set up for MEGA
+int motor1pin1 = 2;           // pin 2 on L293D
+int motor1pin2 = 3;           // pin 7 on L293D
+int motor2pin1 = 4;           // pin 10 on L293D
+int motor2pin2 = 5;           // pin 15 on L293D
+int enable1pin = 6;           // pin 1 on L293D
+int enable2pin = 7;           // pin 9 on L293D
 
 void setup()
 {
@@ -36,6 +42,14 @@ void setup()
   card.PCD_Init();
   card.PCD_DumpVersionToSerial();
   myServo.attach(servoPin);
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  pinMode(enable1pin, OUTPUT);
+  pinMode(enable2pin, OUTPUT);
+  analogWrite(enable1pin, 150);
+  analogWrite(enable2pin, 150);
 }
 
 void loop()
@@ -150,18 +164,35 @@ String readRFIDUID()
   return fromRFIDString.length() != 0 ? fromRFIDString : previousRFIDUID;
 }
 
+void moveForward()
+{
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+}
+
 void pickItem()
 {
-  for (int i = 0; i <= 150; i += 10)
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, LOW);
+  delay(4000);
+  for (int i = 0; i <= 150; i += 45)
   {
     myServo.write(i);
-    delay(100);
+    delay(50);
   }
-  for (int i = 150; i >= 0; i -= 10)
+  for (int i = 150; i >= 0; i -= 45)
   {
     myServo.write(i);
-    delay(100);
+    delay(50);
   }
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
 }
 
 void makeStartupNoiseUsingServo()
